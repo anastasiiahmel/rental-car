81002443afb56278cf317ab9ae0aab18208b466f
anastasiiahmel <126502776+anastasiiahmel@users.noreply.github.com>
2024-01-31 17:43:59 +0200

    add a build

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
+import SharedLayout from 'components/SharedLayout/SharedLayout';
+import FirstPage from 'pages/FirstPage/FirstPage';
+import SecondPage from 'pages/SecondPage/SecondPage';
+import { AppWrapper } from './App.styled';
+
+
+function App() {
+  return (
+    <AppWrapper>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route path="/first" element={<FirstPage />} />
+          <Route path="/second" element={<SecondPage />}>
+          </Route>
+
+        </Route>
+      </Routes>
+    </AppWrapper>
+  );
+}
+export default App;
---
+++ src/pages/SecondPage/SecondPage.jsx
@@ -0,0 +1,13 @@
+import { Container, Block, Title } from './SecondPage.styled';
+
+const SecondPage = () => {
+  return (
+    <Container>
+      <Block>
+        <Title>Second Page</Title>
+      </Block>
+    </Container>
+  );
+};
+
+export default SecondPage;
