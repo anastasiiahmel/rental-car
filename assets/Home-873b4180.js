067e4ebcfb10cb5758978979f8c7bfe2f8ea3ed2
anastasiiahmel <126502776+anastasiiahmel@users.noreply.github.com>
2024-02-04 21:34:46 +0000

    Deploying to gh-pages from @ anastasiiahmel/rental-car@884b0fa5cc63383eefb2e913dc6c921e47d6ad8c 🚀

---
+++ assets/Home-873b4180.js
@@ -0,0 +1,38 @@
+import{L as o,j as e}from"./index-1a532b96.js";import{n as r}from"./emotion-styled.browser.esm-6a8a3598.js";const t="/rental-car/assets/ImgRentalCar-fdf5eea6.webp",n=r.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-image: url(${t});
+  background-repeat: no-repeat;
+
+  background-size: cover;
+  background-position: center;
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 600px;
+  overflow: hidden !important;
+`,a=r.h1`
+  margin-top: 50px;
+  color:#fff;
+`,p=r.p`
+font-size: 30px;
+`,i=r(o)`
+  display: flex;
+  width: 140px;
+  height: 2px;
+  padding: 24px 10px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 12px;
+  background: #734d26;
+  color: #fff;
+  margin-top: 50px;
+  font-size: 14px;
+  text-transform: uppercase;
+  text-decoration: none;
+  box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
+    0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
+  &:hover {
+    background-color: #4d0f00;}
+`;function c(){return e.jsxs(n,{children:[e.jsx(a,{children:"Go to a free life!"}),e.jsx(p,{children:"And we will help you choose the right wheels "}),e.jsx(i,{to:"/catalog",children:"Order a car"})]})}export{c as default};
